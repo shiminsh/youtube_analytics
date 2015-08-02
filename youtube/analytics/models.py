76d2955f31cdce918f0af44e282468e605c24f4b
Shalini Roy <shaliniroy012@gmail.com>
2015-08-02 21:33:52 +0530

    Added script for fetching youtube channels and details

--- youtube/analytics/models.py
+++ youtube/analytics/models.py
@@ -2,8 +2,33 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
-class ChannelId(models.Model):
-	channel_id = models.CharField(_('Channel Id'), max_length=40)
+class ChannelDetails(models.Model):
+    ch_id = models.CharField(_('Channel Id'), max_length=40, unique=True)
+    name = models.CharField(_('Channel Name'), max_length=100, blank=True, null=True)
+    pic = models.TextField(_('Chanel Picture'), blank=True, null=True)
+    views = models.CharField(_('Views'), max_length=100, blank=True, null=True)
+    subscriber = models.CharField(_('Subscriber'), max_length=100, blank=True, null=True)
+    fb_link = models.CharField(_('Facebook link'), max_length=250, blank=True, null=True)
+    twitter_link = models.CharField(_('Twitter link'), max_length=250, blank=True, null=True)
+    date_joined = models.CharField(_('Joined Date'), max_length=100, blank=True, null=True)
+    status = models.BooleanField(default=False)
+    fetched = models.BooleanField(default=False)
+    subs_limit = models.BooleanField(default=False) 
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
+
+    @property
+    def ch_url(self):
+        url = "https://www.youtube.com/channel/" + str(self.ch_id)
+        return url
+
+    @property
+    def about_url(self):
+        ab_url = "https://www.youtube.com/channel/" + str(self.ch_id) + "/about"
+        return ab_url
+
+    @property
+    def channels_url(self):
+        chs_url = "https://www.youtube.com/channel/" + str(self.ch_id) + "/channels"
+        return chs_url
---
+++ youtube/analytics/parser.py
@@ -0,0 +1,104 @@
+import re
+import requests
+import base64
+
+from bs4 import BeautifulSoup
+from analytics.models import *
+from urlparse import urlparse
+
+class ParseChannel:
+
+    def __init__(self):
+        self.homeUrl = "https://www.youtube.com"
+
+    def initialize_db(self):
+        data = requests.get(self.homeUrl)
+        soup = BeautifulSoup(data.text)
+        links = soup.find_all('a', href=re.compile('/channel/'))
+        distinct_ids = []
+        for link in set(link.get('href') for link in links):
+            channel_id = str(link).lstrip('/channel/')
+            distinct_ids.append(channel_id)
+            print "Saving Channel Id -> " + channel_id
+            obj = ChannelDetails(ch_id=channel_id)
+            obj.save()
+
+    def insert_details(self):
+        status = ChannelDetails.objects.filter(status=False)
+        for x in status:
+            data = requests.get(str(x.about_url))
+            soup = BeautifulSoup(data.text)
+            image = soup.find('img', class_="channel-header-profile-image")
+            img_url = str(image['src'])
+            response = request.get(img_url)
+            img_url = base64.b64encode(response.content)
+            name = soup.find('a', class_="branded-page-header-title-link")
+            name = name.string
+            stats = soup.find_all('span', class_="about-stat")
+            subscriber = None
+            if len(stats) > 1:
+                subscriber = "".join(str(stats[0].b.string).split(","))
+                if int(subscriber) > 400:
+                    x.subs_limit = True
+            date = stats[-1].string
+            views = None
+            if len(stats) > 2:
+                views = "".join(str(stats[1].b.string).split(","))
+            links = soup.find_all('a', class_="about-channel-link")
+            twit_link = None
+            fb_link = None
+            for link in links:
+                try:
+                    if 'Twitter' in str(link['title']):
+                        print "twit-> ", str(link['href'])
+                        twit_link = str(link['href'])
+                except:
+                    pass
+                try:
+                    if 'Facebook' in str(link['title']):
+                        print "fb_link-> ", str(link['href'])
+                        fb_link = str(link['href'])
+                except:
+                    pass
+            x.name = name
+            x.pic = img_url
+            x.views = views
+            x.subscriber = subscriber
+            x.fb_link = fb_link
+            x.twitter_link = twit_link
+            x.twitter_link = twit_link
+            x.date_joined = date
+            x.status = True
+            x.save()
+
+    def fetch_channels(self):    
+        fetched = ChannelDetails.objects.filter(fetched = False)
+        for p in fetched:
+            print "url", p.channels_url
+            print "id-> " , p.ch_id
+            data = requests.get(str(p.channels_url))
+            soup = BeautifulSoup(data.text)
+            links = soup.find_all('a', href=re.compile('/channel/'))
+            distinct_ids = []
+            for link in set(link.get('href') for link in links):
+                parsed = urlparse(link)
+                channel_id = str(parsed.path).lstrip('/channel/')
+                channel_id = channel_id.split("/")
+                if len(channel_id) == 1:
+                    channel_id = channel_id[0]
+                    distinct_ids.append(channel_id)
+                    distinct_ids = list(set(distinct_ids))
+            p.fetched = True
+            p.save()
+            for q in distinct_ids:
+                try:
+                    obj = ChannelDetails(ch_id=channel_id)
+                    obj.save()
+                except:
+                    pass
+            
+            
+        
+
+
+
